037effd61785309f2a625337bcd8b30c7ccd4f5f
jbrorson <josefine.brorson@gmail.com>
2021-04-08 14:44:17 +0200

    Routing works fine

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { Switch, Route } from 'react-router-dom';
+import Navbar from './Components/Navbar';
+import Home from './Pages/Home';
+import About from './Pages/About';
+import Contact from './Pages/Contact';
+import LatestWork from './Pages/LatestWork';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="sidebar">
+        <Navbar />
+      </div>
+      <div className="main-content">
+        <div className="content">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/contact" component={Contact} />
+            <Route path="/work" component={LatestWork} />
+          </Switch>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/Pages/LatestWork.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function LatestWork() {
+  return (
+    <div>
+      My portfolio
+    </div>
+  )
+}
+
+export default LatestWork;
