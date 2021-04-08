c9c0b648c1dd0846a8240e98d065b443f8abd50c
jbrorson <josefine.brorson@gmail.com>
2021-04-08 14:28:02 +0200

    Pages on website

---
+++ src/Pages/Home.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+
+function Home() {
+  return (
+    <div className="Home">
+      <header className="hero">
+        <h1 className="hero-text">
+          Hello, I am Josefine
+          {/* <span> Brorson</span> */}
+        </h1>
+        <p className="h-sub-text">
+          Hej alla jag är Josefine, en frontend utvecklare baserad i vackra stockholm, sverige.
+          Hej alla jag är Josefine, en frontend utvecklare baserad i vackra stockholm, sverige.
+          Hej alla jag är Josefine, en frontend utvecklare baserad i vackra stockholm, sverige.
+          Hej alla jag är Josefine, en frontend utvecklare baserad i vackra stockholm, sverige.
+        </p>
+        <div className="icons">
+          <FontAwesomeIcon icon={faGithub} className="github" />
+          <FontAwesomeIcon icon={faLinkedin} className="linkedin" />
+        </div>
+      </header>
+    </div>
+  )
+}
+
+export default Home;
