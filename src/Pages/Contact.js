171318c641819ab9f1c58be3fb2c09f214cd5262
jbrorson <josefine.brorson@gmail.com>
2021-04-09 16:20:36 +0200

    Built first version of contact form

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -1,17 +1,48 @@
 import React from 'react'
 import Title from '../Components/Title';
+import avatar from '../images/avatar.jpg';
 
 function Contact() {
   return (
     <div className="Contact">
       <Title title={"Contact"} span="Contact" />
-      <form className="form">
-        {/* <h1>Get in touch with me</h1> */}
-        <div>
-          <p>Email</p>
-          <input type="email" name="text"></input>
-        </div>
-      </form>
+      <div className="form-wrapper">
+        <form className="form">
+          <div className="form-grid">
+            <p>
+              <label>Name</label>
+              <input
+                type="text"
+                name="text"
+                placeholder="Name">
+              </input>
+            </p>
+            <p>
+              <label>Email</label>
+              <input
+                type="email"
+                name="text"
+                placeholder="Email">
+              </input>
+            </p>
+            <p>
+              <label>Message</label>
+              <textarea
+                className="textarea"
+                type="text"
+                placeholder="Message">
+              </textarea>
+            </p>
+          </div>
+          <div className="btn-holder">
+            <button type="submit">Send message</button>
+          </div>
+
+
+        </form>
+        <div className="form-img"></div>
+        {/* <img src={avatar} /> */}
+      </div>
     </div>
   )
 }
