c21dfc24da0185e6cdd325687b939c8c90b0a140
jbrorson <josefine.brorson@gmail.com>
2021-04-14 13:36:17 +0200

    New styling

--- src/Components/timelineElements.js
+++ src/Components/timelineElements.js
@@ -38,6 +38,24 @@ let timelineElements = [
     date: "FEB19-APR19",
     icon: "education",
   },
+  {
+    id: 5,
+    title: "EXAM - Frontend Developer",
+    companyName: "KYH",
+    description: "Graduated!! After 1,5 years of study I have now finished my polytechnic education.",
+    location: "Stockholm, Sweden",
+    date: "AUG19 - MAY21",
+    icon: "education",
+  },
+  {
+    id: 6,
+    title: "Course in C#",
+    companyName: "Nackademin",
+    description: "This was my first step to become a developer. I started my jurney with a basic C# course.",
+    location: "Stockholm, Sweden",
+    date: "FEB19-APR19",
+    icon: "education",
+  },
 ];
 
 export default timelineElements;
--- src/Pages/Cv.js
+++ src/Pages/Cv.js
@@ -11,35 +11,39 @@ import "react-vertical-timeline-component/style.min.css";
 
 function Cv() {
   let workIconStyles = { background: "#06D6A0" };
-  let eduIconStyles = { background: "#f9c74f" };
+  let eduIconStyles = { background: "#3da3d5" };
 
   return (
-    <div className="Timeline">
+    <>
       <Title title={"Timelime"} span={"Timeline"} />
-      <VerticalTimeline>
-        {timelineElements.map((element) => {
-          let isWorkIcon = element.icon === "work";
+      <div className="Timeline">
 
-          return (
-            <VerticalTimelineElement
-              key={element.key}
-              date={element.date}
-              dateClassName="date"
-              iconStyle={isWorkIcon ? workIconStyles : eduIconStyles}
-              icon={isWorkIcon ? <WorkIcon /> : <EduIcon />}
-            >
-              <h3 className="vertical-timeline-element-title">
-                {element.title}
-              </h3>
-              <h5 className="vertical-timeline-element-subtitle">
-                {element.location}
-              </h5>
-              <p id="description">{element.description}</p>
-            </VerticalTimelineElement>
-          );
-        })}
-      </VerticalTimeline>
-    </div>
+        <VerticalTimeline>
+          {timelineElements.map((element) => {
+            let isWorkIcon = element.icon === "work";
+
+            return (
+              <VerticalTimelineElement
+                // style={{ left: "15%", }}
+                key={element.key}
+                date={element.date}
+                dateClassName="date"
+                iconStyle={isWorkIcon ? workIconStyles : eduIconStyles}
+                icon={isWorkIcon ? <WorkIcon /> : <EduIcon />}
+              >
+                <h3 className="vertical-timeline-element-title">
+                  {element.title}
+                </h3>
+                <h5 className="vertical-timeline-element-subtitle">
+                  {element.location}
+                </h5>
+                <p id="description">{element.description}</p>
+              </VerticalTimelineElement>
+            );
+          })}
+        </VerticalTimeline>
+      </div>
+    </>
   )
 }
 
