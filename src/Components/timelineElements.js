bec113ee06ca0173003e054d6aad3d1b933557ca
jbrorson <josefine.brorson@gmail.com>
2021-04-14 11:40:53 +0200

    timeline component added to display my resume as timeline

---
+++ src/Components/timelineElements.js
@@ -0,0 +1,43 @@
+// Flyttas över till firebase när det fungerar...
+
+
+let timelineElements = [
+  {
+    id: 1,
+    title: "Intern, Frontend Developer",
+    companyName: "KYH Stockholm",
+    description: "I did my second part of LIA on my school by buildning an application for them.",
+    location: "Stockholm, Sweden",
+    date: "JAN21 - APR21",
+    icon: "work",
+  },
+  {
+    id: 2,
+    title: "Intern, Frontend Developer",
+    companyName: "Talent Venture Group",
+    description: "I did my first part of my LIA (internship) on TVG which is a investment company based in Stockholm. I worked in two seperate teams during my time there.",
+    location: "Stockholm, Sweden",
+    date: "OCT20 - JAN21",
+    icon: "work",
+  },
+  {
+    id: 3,
+    title: "EXAM - Frontend Developer",
+    companyName: "KYH",
+    description: "Graduated!! After 1,5 years of study I have now finished my polytechnic education.",
+    location: "Stockholm, Sweden",
+    date: "AUG19 - MAY21",
+    icon: "education",
+  },
+  {
+    id: 4,
+    title: "Course in C#",
+    companyName: "Nackademin",
+    description: "This was my first step to become a developer. I started my jurney with a basic C# course.",
+    location: "Stockholm, Sweden",
+    date: "FEB19-APR19",
+    icon: "education",
+  },
+];
+
+export default timelineElements;
